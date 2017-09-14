6df6756c6231395084dd1652c4954dab42cd2588
dalton-lee <li.docx@gmail.com>
2017-09-14 20:40:25 +0800

    [fix]函数位置，变量名词

--- shell/.rm.sh
+++ shell/.rm.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#当前日期
+today=`date "+%Y%m%d"`
+
 # 设置回收路径
 trashPath=$HOME/.Trash
 
@@ -12,24 +15,13 @@ fi
 args=${@##-*}
 
 # 移动文件并备份，如有必要，修改错误信息并输出
-msg=`mv --backup=t $args "$dest" 2>&1`
+msg=`mv --backup=t $args $trashPath 2>&1`
 if [ ! "$msg" = "" ]; then
-    echo ${msg//mv/my_rm}  # 将错误信息中的“mv”替换成"my_rm"
+    echo ${msg//mv/.rm.sh}  # 将错误信息中的“mv”替换成".rm.sh"
 fi
 
-# ------------------------------
 # 设置标志文件路径
 flagFile=$trashPath/.flag
-
-today=`date "+%Y%m%d"`
-if [ -f "$flagFile" ]; then
-    lastDate=`cat $flagFile`  # 读取标志文件中的日期
-    if [ "$lastDate" -ne "$today" ]; then  # 如果不是今天的日期就清理
-        clearOldFile
-    fi
-else  # 标志文件不存在也要清理
-    clearOldFile
-fi
 
 # ======================================
 # 每天第一次使用这个命令后，清理一段时间以前回收的文件
@@ -41,3 +33,12 @@ function clearOldFile()
     find $trashPath -maxdepth 1 -ctime +7 |xargs /bin/rm -rf
     echo $today > $flagFile # 输出今天的日期到标志文件
 }
+
+if [ -f "$flagFile" ]; then
+    lastDate=`cat $flagFile`  # 读取标志文件中的日期
+    if [ "$lastDate" -ne "$today" ]; then  # 如果不是今天的日期就清理
+        clearOldFile
+    fi
+else  # 标志文件不存在也要清理
+    clearOldFile
+fi
